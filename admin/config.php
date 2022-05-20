220876c71629f8d92a4593e061ba8b23d2718474
Jester <justjestervz@gmail.com>
2022-05-20 16:00:01 +0300

    init

---
+++ admin/config.php
@@ -0,0 +1,34 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://dev.shinyshawty.ru/admin/');
+define('HTTP_CATALOG', 'http://dev.shinyshawty.ru/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://dev.shinyshawty.ru/admin/');
+define('HTTPS_CATALOG', 'http://dev.shinyshawty.ru/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/OpenServer/domains/dev.shinyshawty.ru/admin/');
+define('DIR_SYSTEM', 'C:/OpenServer/domains/dev.shinyshawty.ru/system/');
+define('DIR_IMAGE', 'C:/OpenServer/domains/dev.shinyshawty.ru/image/');
+define('DIR_LANGUAGE', 'C:/OpenServer/domains/dev.shinyshawty.ru/admin/language/');
+define('DIR_TEMPLATE', 'C:/OpenServer/domains/dev.shinyshawty.ru/admin/view/template/');
+define('DIR_CONFIG', 'C:/OpenServer/domains/dev.shinyshawty.ru/system/config/');
+define('DIR_CACHE', 'C:/OpenServer/domains/dev.shinyshawty.ru/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/OpenServer/domains/dev.shinyshawty.ru/system/storage/download/');
+define('DIR_LOGS', 'C:/OpenServer/domains/dev.shinyshawty.ru/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/OpenServer/domains/dev.shinyshawty.ru/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/OpenServer/domains/dev.shinyshawty.ru/system/storage/upload/');
+define('DIR_CATALOG', 'C:/OpenServer/domains/dev.shinyshawty.ru/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'shiny_general');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCartForum API
+define('OPENCARTFORUM_SERVER', 'https://opencartforum.com/');
---
+++ config.php
@@ -0,0 +1,33 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://dev.shinyshawty.ru/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://dev.shinyshawty.ru/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/OpenServer/domains/dev.shinyshawty.ru/catalog/');
+define('DIR_SYSTEM', 'C:/OpenServer/domains/dev.shinyshawty.ru/system/');
+define('DIR_IMAGE', 'C:/OpenServer/domains/dev.shinyshawty.ru/image/');
+define('DIR_LANGUAGE', 'C:/OpenServer/domains/dev.shinyshawty.ru/catalog/language/');
+define('DIR_TEMPLATE', 'C:/OpenServer/domains/dev.shinyshawty.ru/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/OpenServer/domains/dev.shinyshawty.ru/system/config/');
+define('DIR_CACHE', 'C:/OpenServer/domains/dev.shinyshawty.ru/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/OpenServer/domains/dev.shinyshawty.ru/system/storage/download/');
+define('DIR_LOGS', 'C:/OpenServer/domains/dev.shinyshawty.ru/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/OpenServer/domains/dev.shinyshawty.ru/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/OpenServer/domains/dev.shinyshawty.ru/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'shiny_general');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// Redis
+// define('CACHE_HOSTNAME', 'localhost');
+// define('CACHE_PORT', '6379');
+// define('CACHE_PREFIX', 'oc_');
